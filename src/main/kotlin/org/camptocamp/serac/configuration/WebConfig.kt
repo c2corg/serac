a80d7c736251cedab8925129a3e74c587921d573
Bruno Besson <bruno.besson@viseo.com>
2019-11-22 14:07:52 +0100

    Configure CORS

---
+++ src/main/kotlin/org/camptocamp/serac/configuration/WebConfig.kt
@@ -0,0 +1,15 @@
+package org.camptocamp.serac.configuration
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.EnableWebMvc
+
+@Configuration
+@EnableWebMvc
+class WebConfig(@Value("\${cors.origins}") private val allowedOrigins: Array<String>) : WebMvcConfigurer {
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**").allowedOrigins(*this.allowedOrigins)
+    }
+}
